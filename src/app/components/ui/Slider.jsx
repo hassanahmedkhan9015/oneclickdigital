d1c04a3db5105f9abe9b8aec82a85b2c72f9eb73
Hassan Ahmed Khan <hassankhan108017@gmail.com>
2024-02-15 18:13:20 +0500

    build warning solve

--- src/app/components/ui/Slider.jsx
+++ src/app/components/ui/Slider.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import Image from "next/image";
-// import SlideOne from "@public/assets/Hero/heroone.svg";
 import SlideTwo from "@public/assets/Hero/herotwo.svg";
 import SlideThree from "@public/assets/Hero/herothree.svg";
 import SlideFour from "@public/assets/Hero/herofour.svg";
@@ -11,15 +10,13 @@ const Slider = () => {
   const changeTime = 4000; // Change image every 4 seconds (4000 milliseconds)
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      showNextSlide();
-    }, changeTime);
-    return () => clearInterval(interval);
-  }, [slideIndex]);
+    const showNextSlide = () => {
+      setSlideIndex((prevIndex) => (prevIndex + 1) % slides.length);
+    };
 
-  const showNextSlide = () => {
-    setSlideIndex((prevIndex) => (prevIndex + 1) % slides.length);
-  };
+    const interval = setInterval(showNextSlide, changeTime);
+    return () => clearInterval(interval);
+  }, [slides.length]);
 
   return (
     <div className="slider-container relative overflow-hidden">
@@ -28,7 +25,7 @@ const Slider = () => {
           key={index}
           className={`slide ${index === slideIndex ? "visible" : "hidden"}`}
         >
-          <Image src={slide}  alt="slide_image"/>
+          <Image src={slide} alt="slide_image" />
         </div>
       ))}
     </div>
